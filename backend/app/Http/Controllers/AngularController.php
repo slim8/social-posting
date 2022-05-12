a55bb46785235c7200123d037dde566e4007c049
Zied Maaloul <zied.maaloul@softtodo.com>
2022-05-12 15:10:39 +0100

    [TASK] link angular with laravel on root path

---
+++ backend/app/Http/Controllers/AngularController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AngularController extends Controller
+{
+    public function index()
+    {
+        return view('angular');
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\functions\RoutersController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AngularController;
+use App\Http\Controllers\functions\RoutersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
+
 
 // Route::group(['middleware' => ['role:user']], function () {
 //     Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->name('dashboard');
