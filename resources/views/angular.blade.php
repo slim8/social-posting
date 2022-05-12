4561fa59176bca16d2c6ec9b3bbcf1288a55b5a9
Zied Maaloul <zied.maaloul@softtodo.com>
2022-05-12 16:22:59 +0100

    [TASK] fix routes and httaccess for angular on laravel

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html><html lang="en"><head>
-  <meta charset="utf-8">
-  <title>Frontend</title>
-  <base href="/">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="stylesheet" href="/assets/angular/styles.ef46db3751d8e999.css"></head>
-<body>
-  <app-root></app-root>
-<script src="/assets/angular/runtime.aaedba49815d2ab0.js" type="module"></script><script src="/assets/angular/polyfills.875a1d061b093eff.js" type="module"></script><script src="/assets/angular/main.283dd7594bac315e.js" type="module"></script>
+    <meta charset="utf-8">
+    <title>Example</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <script type="text/javascript" src="https://connect.facebook.net/en_US/sdk.js"></script>
+  <style>html,body{width:100%;height:100%}*,*:before,*:after{box-sizing:border-box}html{font-family:sans-serif;line-height:1.15;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;-ms-overflow-style:scrollbar;-webkit-tap-highlight-color:rgba(0,0,0,0)}body{margin:0;color:#000000d9;font-size:14px;font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,"Apple Color Emoji","Segoe UI Emoji",Segoe UI Symbol,"Noto Color Emoji";font-variant:tabular-nums;line-height:1.5715;background-color:#fff;font-feature-settings:"tnum","tnum"}html{--antd-wave-shadow-color:#1890ff;--scroll-bar:0}</style><link rel="stylesheet" href="/assets/angular/styles.0b9600b677ca154d.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="/assets/angular/styles.0b9600b677ca154d.css"></noscript></head>
+  <body>
+    <app-root></app-root>
+  <script src="/assets/angular/runtime.b0c43768c5ffd32e.js" type="module"></script><script src="/assets/angular/polyfills.bb7cf1d1dddd3ff9.js" type="module"></script><script src="/assets/angular/main.541cc7fc39073b74.js" type="module"></script>
 
 </body></html>
