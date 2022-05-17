4132405483b222c7797575b27b22c4cb72b021ff
Zied Maaloul <zied.maaloul@softtodo.com>
2022-05-17 08:48:00 +0100

    [TASK] save autorized facebook pages

--- database/migrations/2022_05_16_150138_update_account_facebook.php
+++ database/migrations/2022_05_16_150138_update_account_facebook.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('accounts', function (Blueprint $table) {
             $table->string('uid');
-            $table->string('profilePicture');
+            $table->text('profilePicture');
             $table->string('category');
         });
     }
