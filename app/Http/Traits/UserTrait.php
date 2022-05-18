ad1c97747f2217ed523e427a035e9f8e62b777d7
Zied Maaloul <zied.maaloul@softtodo.com>
2022-05-18 13:41:53 +0100

    [FIX] link Facebook Account with Provider Token

--- app/Http/Traits/UserTrait.php
+++ app/Http/Traits/UserTrait.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Traits;
 
+use App\Models\ProviderToken;
 use Illuminate\Support\Facades\Auth;
 
 trait UserTrait
 {
+    /**
+     * get current Company Id.
+     */
     public static function getCompanyId()
     {
         return Auth::user()->company_id;
+    }
+
+    /**
+     * Get Current user Id.
+     */
+    public static function getCurrentAdminId()
+    {
+        return Auth::user()->id;
+    }
+
+    /**
+     * Get Current TokenProvider Id.
+     */
+    public static function getCurrentProviderId()
+    {
+        $account = ProviderToken::where('created_by', UserTrait::getCurrentAdminId())->first();
+
+        if (!$account) {
+            return false;
+        }
+
+        return $account->id;
     }
 }
