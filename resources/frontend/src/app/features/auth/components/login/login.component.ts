8404e739c8d297bab499a68feb1e9b8dcbde99a1
Oussema Kassis <oussema.kassis@softtodo.com>
2022-05-18 09:14:27 +0100

    [TASK] add token interceptor

--- resources/frontend/src/app/features/auth/components/login/login.component.ts
+++ resources/frontend/src/app/features/auth/components/login/login.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { AuthService } from '../../services/auth.service';
-
+import {Router} from "@angular/router"
+interface success {
+    message:String;
+    token: String;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
+
 export class LoginComponent implements OnInit {
   credentials = {
     email: 'eve.holt@reqres.ifffn',
@@ -16,17 +21,22 @@ export class LoginComponent implements OnInit {
   password?: string;
   constructor(
     private service: AuthService,
-    private message: NzMessageService
+    private message: NzMessageService,
+    private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    localStorage.removeItem('token');
+  }
 
   login() {
     this.service.login(this.credentials).subscribe(
-      (success) => {
+      (success:any) => {
         this.createMessage('success', 'login succeed !');
+        localStorage.setItem('token', success.token);
+        this.router.navigate(['/home']);
       },
-      (error: any) => {
+      (error) => {
         this.createMessage('error', error.error.message);
       }
     );
