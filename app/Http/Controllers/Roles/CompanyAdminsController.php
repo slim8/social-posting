fe8f27a2cfeed0d640d0c508924ed2ec5787648e
Ali Werghemmi <ali.werghemmi@softtodo.com>
2022-05-19 10:36:16 +0100

    [TASK] setup criteria object

---
+++ app/Http/Controllers/Roles/CompanyAdminsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Roles;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
+class CompanyAdminsController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getUsers()
+    {
+        return User::all();
+    }
+}
