dad324e2c543d7be42870c85b97bf3f3e06e7876
Oussema Kassis <oussema.kassis@softtodo.com>
2022-05-24 14:03:19 +0100

    [TASK] remove test upload component

--- resources/frontend/src/app/features/facebook-social/facebook-social-routing.module.ts
+++ resources/frontend/src/app/features/facebook-social/facebook-social-routing.module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { FacebookSocialComponent } from './facebook-social.component';
 import { FacebookPagesComponent } from './components/facebook-pages/facebook-pages.component';
 import { CreatePostComponent } from './components/create-post/create-post.component';
-import { TestUploadComponent } from './components/test-upload/test-upload.component';
 
 const routes: Routes = [
     { path: '', component: FacebookSocialComponent },
     { path: 'pages', component: FacebookPagesComponent },
-    { path: 'create-post', component: CreatePostComponent },
-    { path: 'test-upload', component: TestUploadComponent }
+    { path: 'create-post', component: CreatePostComponent }
 ];
 
 @NgModule({
