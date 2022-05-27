60c72e297b32f0184b0794c1d3fb6c576a985052
Oussema Kassis <oussema.kassis@softtodo.com>
2022-05-27 10:32:42 +0100

    [FIX] view meta pages style

--- resources/frontend/src/app/features/facebook-social/components/facebook-pages/facebook-pages.component.ts
+++ resources/frontend/src/app/features/facebook-social/components/facebook-pages/facebook-pages.component.ts
@@ -4,14 +4,6 @@ import { FacebookSocialService } from '../../services/facebook-social.service';
 import { SharedModule } from '../../../../shared/shared.module';
 import {Router} from "@angular/router";
 
-interface Page {
-    id: Number;
-    pageName: String;
-    pagePictureUrl: String;
-    category: String;
-    pageId: Number;
-}
-
 @Component({
     selector: 'app-facebook-pages',
     templateUrl: './facebook-pages.component.html',
@@ -26,7 +18,12 @@ export class FacebookPagesComponent implements OnInit {
                 private router: Router) { }
 
     pages: any[] = [];
+    fbPageCount = 0
+    instaPageCount = 0
+    instaPages: any[] = [];
+    facebookPages: any[] = [];
     container = [];
+    loading = true;
 
     ngOnInit(): void {
         this.getPages();
@@ -43,6 +40,15 @@ export class FacebookPagesComponent implements OnInit {
             },
             complete: () => {
                 console.log('complete');
+                this.container.forEach((page : any) => {
+                    if(page.provider == 'facebook') {
+                        this.facebookPages.push(page)
+                    } else if(page.provider == 'instagram') {
+                        this.instaPages.push(page)
+                    }
+                })
+                this.instaPageCount = this.instaPages.length
+                this.fbPageCount = this.facebookPages.length
                 this.pages = this.container
             }
         })
