a23ca45c9802a452ba31dcb4efed7ce8dae7d09b
Oussema Kassis <oussema.kassis@softtodo.com>
2022-05-31 16:45:12 +0100

    [TASK] add user login menu and accounts managment component + edit side menu and routes

---
+++ resources/frontend/src/app/features/social-accounts/components/accounts-management/accounts-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accounts-management',
+  templateUrl: './accounts-management.component.html',
+  styleUrls: ['./accounts-management.component.scss']
+})
+export class AccountsManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
