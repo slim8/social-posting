72691cff817c299421f1ea50c7e29f24337f27f9
Zied Maaloul <zied.maaloul@softtodo.com>
2022-06-07 08:29:31 +0100

    [TASK] extend Model and Has Attributes

--- app/Models/ProviderToken.php
+++ app/Models/ProviderToken.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use App\Http\Extends\ExtendedModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ProviderToken extends Model
+class ProviderToken extends ExtendedModel
 {
     use HasFactory;
 
