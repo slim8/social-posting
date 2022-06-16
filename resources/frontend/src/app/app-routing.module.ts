d51bf6da8c075650fd8b6d94e009f657d711812d
Ali Werghemmi <ali.werghemmi@softtodo.com>
2022-06-16 13:46:40 +0100

    [TASK] Add error pages ( 403 / 404 / 500 )

--- resources/frontend/src/app/app-routing.module.ts
+++ resources/frontend/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './features/auth/services/auth.guard';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { ServerErrorComponent } from './shared/components/server-error/server-error.component';
+import { UnauthorizedComponent } from './shared/components/unauthorized/unauthorized.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/application' },
@@ -8,8 +11,10 @@ const routes: Routes = [
     { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
     { path: 'social-accounts', loadChildren: () => import('./features/social-accounts/social-accounts.module').then(m => m.SocialAccountsModule) },
     { path: 'user', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) },
-    { path: '', pathMatch: 'full', redirectTo: 'application' },
-    { path: '**', pathMatch: 'full', redirectTo: 'application' },
+    { path: 'unauthorized', component: UnauthorizedComponent },
+    { path: 'server-error', component: ServerErrorComponent },
+    { path: 'not-found', component: NotFoundComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'not-found' },
     // { path: '', component: MainComponent },
     // { path: 'facebook', loadChildren: () => import('./features/facebook.module').then(m => m.WelcomeModule) }
 
--- resources/frontend/src/app/shared/shared.module.ts
+++ resources/frontend/src/app/shared/shared.module.ts
@@ -4,15 +4,28 @@ import { DemoNgZorroAntdModule } from './ng-zorro-antd.module';
 import { IconsProviderModule } from './icons-provider.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ServerErrorComponent } from './components/server-error/server-error.component';
 
 @NgModule({
     declarations: [
+        NotFoundComponent,
+        ServerErrorComponent,
+        UnauthorizedComponent
+    ],
+    imports: [
+        DemoNgZorroAntdModule
     ],
     exports: [
         DemoNgZorroAntdModule,
         IconsProviderModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+
+        NotFoundComponent,
+        ServerErrorComponent,
+        UnauthorizedComponent
     ]
 })
 export class SharedModule {
