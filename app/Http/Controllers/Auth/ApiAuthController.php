afa7f8773a480a1f75a2f62ccb1aa2e6bc0a2c12
Zied Maaloul <zied.maaloul@softtodo.com>
2022-06-17 08:37:49 +0100

    [TASK] clean code

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\repositories\UserRepository;
 use App\Http\Traits\MailTrait;
+use App\Http\Traits\RequestsTrait;
 use App\Models\Company;
 // use App\Models\User as ModelsUser;
 use App\Models\User;
@@ -17,7 +18,7 @@ use Illuminate\Support\Str;
 
 class ApiAuthController extends Controller
 {
-    use MailTrait;
+    use MailTrait , RequestsTrait;
 
     protected $userRepository;
 
@@ -86,13 +87,10 @@ class ApiAuthController extends Controller
 
         $user->attachRole('companyadmin');
 
-        MailTrait::index('A new user has been Created <br> <strong>Email:</strong> ' . $request->email . '<br> <strong>Password:</strong>' . $password, $request->email, 'Company Account Created', 'emails.accountCreated');
+       // MailTrait::index('A new user has been Created <br> <strong>Email:</strong> ' . $request->email . '<br> <strong>Password:</strong>' . $password, $request->email, 'Company Account Created', 'emails.accountCreated');
 
-        return response()->json([
-            'success' => true,
-            'password' => $password,
-            'message' => trans('message.company_created_sucess') . $request->email,
-        ], 201);
+        return RequestsTrait::processResponse(true , ['password' => $password,
+        'message' => trans('message.company_created_sucess') . $request->email]);
     }
 
     public function registerUser(Request $request)
@@ -125,10 +123,8 @@ class ApiAuthController extends Controller
 
         $user->attachRole('user');
 
-        return response()->json([
-            'success' => true,
-            'message' => trans('message.user_created_suceess') . $request->email,
-        ], 201);
+        return RequestsTrait::processResponse(true , ['success' => true,
+        'message' => trans('message.user_created_suceess') . $request->email]);
     }
 
     public function login(Request $request)
@@ -168,13 +164,10 @@ class ApiAuthController extends Controller
 
                     $jwt = JWT::encode($token, $secret_key, env('JWT_HASH_ALGORITHME'));
 
-                    return response()->json([
-                        'message' => trans('message.sucess_login'),
-                        'token' => $jwt,
-                        'expireAt' => $expire_claim,
-                        'success' => true,
-                        'roles' => $this->userRepository->getCurrentRoles($user),
-                    ], 200);
+                    return RequestsTrait::processResponse(true , ['message' => trans('message.sucess_login'),
+                            'token' => $jwt,
+                            'expireAt' => $expire_claim,
+                            'roles' => $this->userRepository->getCurrentRoles($user)]);
                 } else {
                     $response = ['message' => trans('message.password_mismatch'), 'status' => false];
 
