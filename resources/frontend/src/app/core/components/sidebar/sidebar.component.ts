92a8f615a54551f2c79a5ed7b9e9e58305aeafcd
Oussema Kassis <oussema.kassis@softtodo.com>
2022-06-20 15:30:28 +0100

    [TASK] dashboard connect user/add pages

--- resources/frontend/src/app/core/components/sidebar/sidebar.component.ts
+++ resources/frontend/src/app/core/components/sidebar/sidebar.component.ts
@@ -27,7 +27,6 @@ export class SidebarComponent implements OnInit {
 
     ngOnInit(): void {
         const sideNav = document.querySelector('.mod-sidebar')?.parentElement?.parentElement;
-        console.log(sideNav);
         const sideNavMenu = document.querySelector('.mod-sidebar');
         sideNav?.addEventListener('mouseover', this.openSidenav);
         sideNav?.addEventListener('mouseleave', this.closeSidenav);
