eb4ec2411fa0fb23c44a4828c72f7a807f46a050
Zied Maaloul <zied.maaloul@softtodo.com>
2022-06-29 14:51:48 +0100

    [TASK] convert Image to JPEG when uploading

--- config/app.php
+++ config/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Facade;
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -155,7 +154,6 @@ return [
     */
 
     'providers' => [
-
         /*
          * Laravel Framework Service Providers...
          */
@@ -194,7 +192,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
@@ -209,8 +206,8 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'Eloquent'   => \App\Http\Extends\ExtendedModel::class,
+        'Eloquent' => \App\Http\Extends\ExtendedModel::class,
+        'Image' => \Intervention\Image\Facades\Image::class,
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
-
 ];
