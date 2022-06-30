5032d561b7c80fb01a67a17065a66c49a5fc1e4a
oussema.kassis <oussema.kassis@softtodo.com>
2022-06-30 16:06:19 +0100

    [TASK] fix temp storage

--- build.js
+++ build.js
@@ -4,3 +4,11 @@ const fs = require('fs');
 fs.copyFile('public/assets/angular/index.html', 'resources/views/angular.blade.php', (err) => {
   if (err) throw err;
 });
+
+
+var dir = './public/storage/temporarStored';
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
