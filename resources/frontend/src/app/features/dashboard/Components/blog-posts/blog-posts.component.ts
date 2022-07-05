287a15356209034d9aefd01491384ce485a6a676
maher boughdiri <maher.boughdiri@softtodo.com>
2022-07-05 11:33:40 +0100

    [TASK] add blog posts style

--- resources/frontend/src/app/features/dashboard/Components/blog-posts/blog-posts.component.ts
+++ resources/frontend/src/app/features/dashboard/Components/blog-posts/blog-posts.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPostsComponent implements OnInit {
 
+  posts = [
+    {img: 'https://cdn.eso.org/images/thumb300y/eso2008a.jpg' , createdAt : '23.06.2022' , title : 'Hervorgehobener Post' ,teaser : 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ' },
+    {img: '' , createdAt : '23.06.2022' , title : 'Gleichzeitig posten' ,teaser : 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ' },
+    {img: 'https://cdn.eso.org/images/thumb300y/eso2008a.jpg' , createdAt : '23.06.2022' , title : 'Gleichzeitig posten' ,teaser : 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ' },
+    {img: 'https://cdn.eso.org/images/thumb300y/eso2008a.jpg' , createdAt : '23.06.2022' , title : 'Gleichzeitig posten' ,teaser : 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ' },
+    {img: 'https://cdn.eso.org/images/thumb300y/eso2008a.jpg' , createdAt : '23.06.2022' , title : 'Gleichzeitig posten' ,teaser : 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ' },
+    {img: 'https://cdn.eso.org/images/thumb300y/eso2008a.jpg' , createdAt : '23.06.2022' , title : 'Gleichzeitig posten' ,teaser : 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ' },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
