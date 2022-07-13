4cebc16143a5bc2c6c0ecc671f72e6824ed9862a
Ali Werghemmi <ali.werghemmi@softtodo.com>
2022-07-13 11:03:17 +0100

    [TASK] test deployment

--- app/Http/Controllers/Repositories/PlanRepository.php
+++ app/Http/Controllers/Repositories/PlanRepository.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Repositories;
 use App\Http\Controllers\Controller;
 use App\Models\Plan;
 
+
 class PlanRepository extends Controller
 {
     public function getById($id)
