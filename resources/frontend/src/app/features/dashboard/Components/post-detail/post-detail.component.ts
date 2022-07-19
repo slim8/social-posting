bf57db12e6bfb9f0cb0379626f690ddb75418e47
maher boughdiri <maher.boughdiri@softtodo.com>
2022-07-19 15:57:07 +0100

    [TASK] add dynamic blog posts

--- resources/frontend/src/app/features/dashboard/Components/post-detail/post-detail.component.ts
+++ resources/frontend/src/app/features/dashboard/Components/post-detail/post-detail.component.ts
@@ -1,4 +1,5 @@
-import { ActivatedRoute } from '@angular/router';
+import { NewsService } from './../../services/news.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,12 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDetailComponent implements OnInit {
 
+  news = {
+    id: 1,
+    title: "",
+    teaser: "",
+    picture: "",
+    date: "2022-04-05 00:00:00",
+    template: '',
+    createdAt: "",
+    updatedAt: "",
+    img : '',
+    text_media_news : [{
+      id: 1,
+      title: "",
+      subtitle: "",
+      description: "",
+      picture: "",
+      newsId: 0,
+      createdAt: "",
+      updatedAt: "" ,
+      img : ''
+    }]
+  }
 
-  constructor(private _activatedRoute: ActivatedRoute) { }
+  constructor(private newsService : NewsService ,private route: ActivatedRoute ,private router: Router) { }
 
   ngOnInit(): void {
-    console.log(this._activatedRoute.paramMap);
-    
+    this.getNews(this.route.snapshot.paramMap.get('id'))
+  }
+
+  getNews(news : any){
+    this.newsService.getNewsById(news).subscribe({
+      next: (event: any) => {
+        console.log(event);
+        if(event.new){
+          this.news = {...event.new , img : JSON.parse(event.new.picture).url , text_media_news : event.new.text_media_news.map((media : any) => ({...media , img : JSON.parse(media.picture).url}))};
+
+        }else{
+          this.router.navigate(['/application/dashboard/blog-posts'])
+        }
+      }
+    })
   }
 
 }
