9620058851f981214a0de95bcc2df68703a93ba3
Zied Maaloul <zied.maaloul@softtodo.com>
2022-07-19 08:52:55 +0100

    [TASK] add html templates for mailer

--- app/Http/Traits/MailTrait.php
+++ app/Http/Traits/MailTrait.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Mail;
 
 trait MailTrait
 {
-    public static function index(string $content, string $mailto, string $mailsubject = 'Notification Mail', string $template = 'emails.notificationMail')
+    public static function index(array $content, string $mailto, string $mailsubject = 'Notification Mail', string $template = 'emails.notificationMail')
     {
         try{
             return Mail::send(new NotificationMail($content, $mailto, $mailsubject, $template));
