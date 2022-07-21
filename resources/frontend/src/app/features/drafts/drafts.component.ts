1ecd3adcd6e0592525c1932d9a26da4a27830028
siwar.zayani <siwar.zayani@softtodo.com>
2022-07-21 15:08:10 +0100

    [TASK] delete draft

--- resources/frontend/src/app/features/drafts/drafts.component.ts
+++ resources/frontend/src/app/features/drafts/drafts.component.ts
@@ -5,6 +5,7 @@ import { HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 const calendarIcon = '<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 7.5V5.25C15 4.42157 14.3284 3.75 13.5 3.75H4.5C3.67157 3.75 3 4.42157 3 5.25V7.5M15 7.5V14.25C15 15.0784 14.3284 15.75 13.5 15.75H4.5C3.67157 15.75 3 15.0784 3 14.25V7.5M15 7.5H3M6 2.25V5.25M12 2.25V5.25" stroke="black" stroke - width="1.5" stroke - linecap="round"/><rect x="4.5" y="9" width="2.25" height="2.25" rx="0.375" fill="black"/><rect x="7.875" y="9" width="2.25" height="2.25" rx="0.375" fill="black"/><rect x="11.25" y="9" width="2.25" height="2.25" rx="0.375" fill="black"/></svg>'
 const arrowIcon = '<svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.08398 1.24992L5.41313 4.92077C5.18532 5.14858 4.81598 5.14858 4.58817 4.92077L0.917318 1.24992" stroke="black" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round"/></svg>'
@@ -19,7 +20,7 @@ export class DraftsComponent implements OnInit {
     posts: any[] = [];
     draftsList: any = [];
 
-    constructor(private iconService: NzIconService, private accountsService: AccountsService, private router: Router, private sharedModule : SharedModule,private message: NzMessageService) {
+    constructor(private iconService: NzIconService, private modal: NzModalService, private accountsService: AccountsService, private router: Router, private sharedModule : SharedModule,private message: NzMessageService) {
         this.iconService.addIconLiteral('ng-zorro:customCalendar', calendarIcon);
         this.iconService.addIconLiteral('ng-zorro:customArrow', arrowIcon);
         if (this.router.url.includes('drafts')) {
@@ -59,13 +60,25 @@ export class DraftsComponent implements OnInit {
         })
     }
 
+    check(event:any, value:string) {
+      if(event.target.checked) {
+            if(!this.draftsList.includes(value)) {
+                this.draftsList.push(value);
+            }
+        } else {
+            if(this.draftsList.includes(value)) {
+                let index = this.draftsList.indexOf(value);
+                this.draftsList.splice(index, 1);
+            }
+        }
+     }
+
     removeDraft() {
         if (this.draftsList.length > 0) {
             const formData: FormData = new FormData();
             this.draftsList.forEach((draft: any) => {
                 formData.append('postsIds[]', draft);
             })
-            confirm('Are you sure you want to delete this item?');
             this.accountsService.removeDrafts(formData).subscribe({
                 next: (event: any) => {
                 },
@@ -75,9 +88,21 @@ export class DraftsComponent implements OnInit {
                     this.getDrafts();
                 }
             })
-        } else {
-            // empty
         }
+    }
+
+    showRemoveDraftModal() {
+      alert();
+      this.modal.confirm({
+        nzTitle: 'Do you really want to disconnect this account?',
+        nzContent: '<b style="color: red;">You will have to connect this acocunt via facebook to reconnect</b>',
+        nzOkText: 'Yes',
+        nzOkType: 'primary',
+        nzOkDanger: true,
+        nzOnOk: () => this.removeDraft(),
+        nzCancelText: 'No',
+        nzOnCancel: () => console.log('Cancel')
+    });
     }
 
 
@@ -105,4 +130,17 @@ export class DraftsComponent implements OnInit {
   createMessage(type: string, message: any): void {
     this.message.create(type, ` ${message}`);
   }
+  test() {
+    this.modal.confirm({
+      nzTitle: 'Do you really want to disconnect this account?',
+      nzContent: '<b style="color: red;">You will have to connect this acocunt via facebook to reconnect</b>',
+      nzOkText: 'Yes',
+      nzOkType: 'primary',
+      nzOkDanger: true,
+      nzOnOk: () => this.removeDraft(),
+      nzCancelText: 'No',
+      nzOnCancel: () => console.log('Cancel')
+    });
+  }
+
 }
